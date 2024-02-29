3f1b13f0924b565a6aa89a0d52a93e961c9ec6a0
reinelic <reine.iramurikiye@clearglobal.org>
2024-02-29 22:37:14 +0200

    created audios

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import NavItems from './NavItems'
 import { useSession, signIn, signOut } from 'next-auth/react'
 
 export const Navbar = () => {
-  const { data: session } = useSession()
+  // const { data: session } = useSession()
 
-  console.log(`session ${session}`)
+  // console.log(`session ${session}`)
 
   return (
     <div className='sticky inset-x-0 top-0 z-50 h-16 bg-white'>
