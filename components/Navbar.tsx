55e8ab9d0ca97457480769dc9536deca991368fb
reinelic <reine.iramurikiye@clearglobal.org>
2024-03-01 14:36:31 +0200

    adding styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import MaxWidthWrapper from './MaxWidthWrapper'
 import NavItems from './NavItems'
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
+import { authConfig } from '../auth'
 
-export const Navbar = () => {
-  // const { data: session } = useSession()
-
-  // console.log(`session ${session}`)
+export const Navbar = async () => {
+  const session = await getServerSession(authConfig)
+  console.log(session)
 
   return (
     <div className='sticky inset-x-0 top-0 z-50 h-16 bg-white'>
